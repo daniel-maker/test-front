a39275040a02010322e1ad52787550583f043ee9
Daniel Biazuzo <danielmaker@gmail.com>
2019-03-05 03:52:05 -0300

    upload-files

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "estilo.css": "estilo.less" 
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['*.less'],
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
